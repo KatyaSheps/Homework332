435ee0528f1329e92916e821cd1d7405344f997f
Katya Sheps <katya@sheps.ru>
2019-06-02 16:44:10 +0300

    Made switcher between language: int position = spinner.getSelectedItemPosition();         if (position == 1) {             locale = new Locale("ru");         } else {             locale = new Locale("eng");         }

--- app/src/main/java/ru/sheps/android/homework332/MainActivity.java
+++ app/src/main/java/ru/sheps/android/homework332/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import java.util.Locale;
 
@@ -16,12 +19,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btnOk(View view) {
-
-        Locale locale = new Locale("ru");
+        Locale locale;
+        Spinner spinner = findViewById(R.id.spinner);
+        int position = spinner.getSelectedItemPosition();
+        if (position == 1) {
+            locale = new Locale("ru");
+        } else {
+            locale = new Locale("eng");
+        }
         Configuration config = new Configuration();
         config.setLocale(locale);
         getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
         recreate();
 
     }
+
 }
